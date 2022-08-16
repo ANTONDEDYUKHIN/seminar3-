51f118d79cf58be77ffa489086a9fa30ead909d2
ANTON <19111984@list.ru>
2022-08-16 21:25:55 +0700

    squareN1

---
+++ Task22-2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написть прог, которая принимает на вход число
+//(Н) и выдает таблицу квадратов чиаел от 1 до Н.
+// 5 - 1, 4, 9, 16, 25. 2 - 1, 4.
+Console.Clear();
+Console.Write("Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (n > 0)
+{
+    while(i <= n)
+    {
+    int q = i * i;
+    Console.WriteLine($"{i} {q}");
+    i++;
+    }
+}
+else Console.WriteLine("Incorrect data entered.");
