8ca5343244c12b4a75c58671586ddf714c6b2bff
ANTON <19111984@list.ru>
2022-08-16 21:07:40 +0700

    numquart

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿//Написать прогр, которая по заданному номеру 
+// четверти, показывает диапазон возм.
+//координат точек в этой четверти (Х и У)
+Console.Clear();
+Console.WriteLine("Enter the number of quarter: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string GetRange(int Q)
+// Console.Write("X: ");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int Y = Convert.ToInt32(Console.ReadLine());
+// string GetQuarter(int X, int Y)
+{
+if (Q == 1) return"X > 0 && Y > 0";
+if (Q == 2) return"X < 0 && Y > 0";
+if (Q == 3) return"X < 0 && Y < 0";
+if (Q == 4) return"X > 0 && Y < 0";
+return"Incorrect data entered.";
+}
+string result = GetRange(quarter);
+Console.WriteLine(result);
